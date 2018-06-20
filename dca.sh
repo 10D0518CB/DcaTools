0fc75878ead55c5758b6153485df62dbdd3bd024
10D0518CB <40421024+10D0518CB@users.noreply.github.com>
2018-06-20 15:09:19 +0000

    Add files via upload

---
+++ dca.sh
@@ -0,0 +1,545 @@
+#!/usr/bin/bash
+#Jangan recode bangsat
+#Gw buat jga mikir anjing
+clear
+blue='\033[34;1m'
+green='\033[32;1m'                                        purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay "*MR_4SWL0*" | lolcat
+toilet -f big -F gay "DCATools" 
+echo ""      
+echo "\033[32;1mCoder BY : DR.D3STr0" 
+echo "\033[35;1mTeam : Demon Cyber Army"
+echo "\033[33;1mKontak Me : +62 823-7784-4088"
+echo "\033[31;1mSubcribe Youtube:http://youtube.com/c/StereoBlaze"
+echo "\033[34;1m[====================================================[>"
+
+sleep 1
+
+echo ""
+echo "\033[32;1mSilahkan Pilih Tools"
+echo "\033[36;1m"
+
+echo "======================================================="
+
+echo $green "1.Lazymux"
+
+echo "======================================================="
+
+echo $red "2.Install Tools *MR_4SWL0*"
+
+echo "======================================================="
+
+echo $cyan "3.Hammer Buat DDOS"
+
+echo "======================================================="
+
+echo $purple "4.Chattan Ala Hacker"
+
+echo "======================================================="
+
+echo $white "5.multi-bruteforce-facebook"
+
+echo "======================================================="
+
+echo $yellow "6.install Xerxes"
+
+echo "======================================================="
+
+echo $blue "7.install Metasploit"
+
+echo "======================================================="
+
+echo $green "8.Termux-ohmyzsh Buat Mempercantik Tampilan Termux"
+
+echo "======================================================="
+
+echo $blue "9.Weeman"
+
+echo "======================================================="
+
+echo $red "10.Webdav"
+
+echo "======================================================="
+
+echo $white "11.Red Hawk"
+
+echo "======================================================="
+
+
+echo $green "12.Install A-Rat"
+
+echo "======================================================="
+
+echo $red "13.Allinwan"
+
+echo "======================================================="
+
+echo $green "14.SpiderBot"
+
+echo "======================================================="
+
+echo $cyan "15.Kali-Ubuntu"
+
+echo "======================================================="
+
+echo $blue "16.Sqlmap"
+
+echo "======================================================="
+
+echo $red "17.Websploit"
+
+echo "======================================================="
+
+echo $cyan "18.Sqlmap"
+
+echo "======================================================="
+
+echo $red "19.Mulung Bitcoin"
+
+echo "======================================================="
+
+echo $green "20.Kawai-Botnet"
+
+echo "======================================================="
+
+echo $purple "0.Keluar"
+
+echo "======================================================="
+
+read -p "Silahkan Di Pilih ea:v>>" p
+
+if [ $p = 1 ]
+
+then
+
+toilet "*MR_4SWL0*"
+
+pkg install python2
+
+pkg install git && pkg install wget
+
+pkg install php && pip2 install requests
+
+pkg install toilet && pip2 install mechanize
+
+pkg install figlet
+
+git clone https://github.com/Gameye98/Lazymux
+
+cd Lazymux
+
+ls
+
+chmod +x lazymux.py
+
+ls
+
+python2 lazymux.py
+
+
+
+fi
+
+if [ $p = 2 ]
+
+then
+
+figlet "*MR_4SWL0*" | lolcat
+
+pkg install php && pkg install toilet
+
+pkg install python2 && pkg install figlet
+
+pkg install ruby && gem install lolcat
+
+git clone https://github.com/MR45WL0/SWTOOLS
+
+cd SWTOOLS
+
+chmod +x SWTOOLS.sh
+
+sh SWTOOLS.SH
+
+fi
+
+if [ $p = 3 ]
+
+then
+
+toilet "*MR_4SWL0*"
+
+pkg update
+
+pkg upgrade
+
+pkg install python
+
+pkg install git
+
+git clone https://github.com/cyweb/hammer
+
+cd hammer
+
+python hammer.py
+
+fi
+
+if [ $p = 4 ]
+
+then
+
+figlet "*MR_4SWL0*" | lolcat
+
+echo $red "Info Selngkapnya Lihat Chanell YouTube Kami"
+
+echo $red "Link : https://youtu.be/qSebUSDzU0g"
+
+sleep 3
+
+pkg install irssi
+
+irssi
+
+sleep 1
+
+/connect chat.freenode.net
+
+fi
+
+if [ $p = 5 ]
+
+then
+
+toilet "*MR_4SWL0*"
+
+pkg install python2
+
+pkg install wget && pkg install git
+
+pip2 install mechanize && pip2 install requests
+
+git clone https://github.com/tikuskecil/multi-bruteforce-facebook
+
+cd multi-bruteforce-facebook
+
+python2 MBF.py
+
+fi
+
+if [ $p = 6 ]
+
+then
+
+figlet "*MR_4SWL0*" | lolcat
+
+pkg install python2 && pkg install php
+
+pkg install git && pkg install clang -y
+
+git clone https://github.com/zanyarjamal/xerxes
+
+fi
+
+if [ $p = 7 ]
+
+then
+
+toilet "*MR_4SWL0*"
+
+pkg install curl
+ 
+curl -LO https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
+
+chmod 7777 metasploit.sh
+
+./metasploit.sh
+
+fi
+
+if [ $p = 8 ]
+
+then
+
+figlet "*MR_4SWL0*" | lolcat
+
+echo  "Untuk Info Selengkapnya Cek Di Chanell YouTube Kami" 
+
+echo  "Link : https://youtu.be/u5FpxxCGP64"
+
+sleep 2
+
+apt install git
+
+git clone https://github.com/Cabbagec/termux-ohmyzsh
+
+cd termux-ohmyzsh
+
+sh install.sh
+
+fi
+
+if [ $p = 9 ]
+
+then
+
+toilet "*MR_4SWL0*"
+
+pkg install python2
+
+pkg install git && pkg install figlet
+
+pkg install php && pkg install toilet
+
+git clone https://github.com/evait-security/weeman.git
+
+cd weeman
+
+python2 weeman.py
+
+fi
+
+if [ $p = 10 ]
+
+then
+
+figlet "*MR_4SWL0*" | lolcat
+
+apt update && apt upgrade
+
+apt install python2
+
+pip2 install urllib3 chardet certifi idna requests
+
+apt install openssl curl
+
+pkg install libcurl 
+
+mkdir webdav
+
+cd webdav
+
+wget https://pastebin.com/raw/HnVyQPtR
+
+mv HnVyQPtR webdav.py
+
+chmod 777 webdav.py
+
+python2 webdav.py
+
+fi
+
+if [ $p = 11 ]
+
+then
+
+toilet "*MR_4SWL0*"
+
+apt update && apt upgrade
+
+apt install git
+
+apt install php
+
+apt install php-curl
+
+apt install php-xml
+
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+
+cd RED_HAWK
+
+php rhawk.php
+
+fi
+
+if [ $p = 12 ]
+
+then
+
+figlet "*MR_4SWL0*" | lolcat
+
+apt update && apt upgrade
+
+apt install python2 git
+
+git clone https://github.com/Xi4u7/A-Rat/ a-rat/
+
+cd a-rat
+
+mv A-Rat.py main.py
+
+chmod +x main.py
+
+python2 main.py
+
+fi
+
+if [ $p = 13 ]
+
+then
+
+toilet "*MR_4SWL0*"
+
+apt update && apt upgrade
+
+apt install wget
+
+wget https://raw.githubusercontent.com/verluchie/allinwan/master/allinwan
+
+chmod 777 allinwan
+
+./allinwan
+
+fi
+
+if [ $p = 14 ]
+
+then
+
+figlet "*MR_4SWL0*" | lolcat
+
+pkg install php
+
+pkg install git
+
+git clone https://github.com/Cvar1984/SpiderBot
+
+cd SpiderBot
+
+php botcoment.php
+
+fi
+
+if [ $p = 15 ]
+
+then
+
+toilet "*MR_4SWL0*"
+
+apt update
+
+apt install git
+
+apt install wget
+
+apt install proot
+
+git clone https://github.com/Neo-Oli/termux-ubu&#8230;
+
+cd termux-ubuntu
+
+chmod +x ubuntu.sh
+
+./ubuntu.sh
+
+./start.sh
+
+fi
+
+if [ $p = 16 ]
+
+then
+
+figlet "*MR_4SWL0*" | lolcat
+
+apt install python2
+
+git clone https://github.com/sqlmapproject/sqlmap.git
+
+cd sqlmap
+
+python2 sqlmap.py -h
+
+fi
+
+if [ $p = 17 ]
+
+then
+
+toilet "*MR_4SWL0*"
+
+apt update && upgrade -y
+
+apt install python2 && pkg install git
+
+pip2 install scapy
+
+git clone https://github.com/The404Hacking/websploit
+
+cd websploit
+
+echo $blue "untuk melihat modul ketik:
+wsf> show modules"
+
+echo $blue "untuk melihat perintah yg tersedia ketik: wsf> help"
+
+sleep 4
+
+python2 websploit.py
+
+fi
+
+if [ $p = 18 ]
+
+then
+
+figlet "*MR_4SWL0*" | lolcat
+
+pkg install git && pkg install python2
+
+pkg install php && pkg install toilet
+
+git clone https://github.com/sqlmapproject/sqlmap.git
+
+cd sqlmap
+
+python2 sqlmap.py
+
+fi
+
+if [ $p = 19 ]
+
+then
+
+toilet "*MR_4SWL0*"
+
+pkg install git && pkg install php && pkg install toilet && pkg install figlet && pkg install python2
+
+git clone https://github.com/Cvar1984/pemulungBTC
+
+cd pemulungBTC
+
+php mulung.php
+
+fi
+
+if [ $p = 20 ]
+
+then
+
+figlet "*MR_4SWL0*" | lolcat
+
+pkg install git 
+
+pkg install python2
+
+git clone https://github.com/Cvar1984/Kawai-Botnet
+
+cd Kawai-Botnet
+
+php kawai.php
+
+fi
+
+if [ $p = 0 ]
+
+then
+
+exit
+
+fi
